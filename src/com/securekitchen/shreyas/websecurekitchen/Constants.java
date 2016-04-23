ec3142deb16ce9aa75fc94b976ed4aa385dfc683
Shreyas Dawkhare <shreyas.dawkhare@gmail.com>
2016-04-24 00:59:48 +0530

    Removed duplicate files

--- src/com/securekitchen/shreyas/websecurekitchen/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.securekitchen.shreyas.websecurekitchen;
-
-public class Constants 
-{
-	public static String HOST_IP = "172.16.86.163";
-	public static String DATABASE_NAME = "securekitchen";
-	public static String SERVER_API_KEY = "AIzaSyALpTkHJC3wGWpHacLPI_HPyHhi0vaoQKw"; 
-
-}
